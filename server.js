e8d8a45fb90483b8d69dac35aea714bd998f29db
pumca <pumca2025@gmail.com>
2025-10-02 19:10:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -434,8 +434,8 @@ app.post("/team-register", async (req, res) => {
     // 7. Send Emails
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
