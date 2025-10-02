b8c438340c4a6b24ac8285b2672f1e370781daf1
pumca <pumca2025@gmail.com>
2025-10-02 20:25:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -449,6 +449,7 @@ app.post("/team-register", async (req, res) => {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+        connectionTimeout: 10000, // 10 seconds
     });
 
     for (const student of studentDocs) {
